65d590f1a97163ccd0b564c19260786951be11be
Georgiy Eresko <zhorik.eresko@mail.ru>
2024-06-29 05:27:32 +0300

    last part of C

---
+++ homeworks/week5/speller/dictionary.c
@@ -0,0 +1,97 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+int no_words = 0;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    int hash_value = hash(word);
+    node *cursor = table[hash_value];
+    while(cursor != NULL) {
+        if (strcasecmp(cursor->word, word) == 0) {
+            return true;
+        }
+        else {
+            cursor = cursor->next;
+        }
+    }
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    return toupper(word[0]) - 'A';
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    for (int i = 0; i < N; i++) {
+        table[i] = NULL;
+    }
+    // Open the dictionary file
+    FILE *dict_file = fopen(dictionary, "r");
+    if (dict_file == NULL) {
+        printf("Can't open that file\n");
+        return false;
+    }
+    char buffer[45];
+
+    // Read each word in the file
+    while (fscanf(dict_file, "%s", buffer) != EOF) {
+        node *new_word = malloc(sizeof(node));
+        int hash_value = hash[buffer];
+        strcpy(new_word->word = buffer);
+        new_word->next = table[hash_value];
+        table[hash_value] = new_word;
+        no_words++;
+    }
+    return true;
+
+    // Close the dictionary file
+    fclose(dict_file);
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return no_words;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    for (int i = 0; i < N; i++) {
+        node *tmp = table[i];
+        node *cursor = table[i];
+        while (cursor != NULL) {
+            cursor = cursor->next;
+            free(tmp);
+            tmp = cursor;
+        }
+    }
+    return true;
+}
