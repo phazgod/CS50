1f90b465b35d2b2b1095ded7c65a74c7c0e36797
Georgiy Eresko <zhorik.eresko@mail.ru>
2024-07-15 23:48:09 +0300

    changes

--- database/SQLite.sql
+++ database/SQLite.sql
@@ -3,5 +3,8 @@
 -- SELECT * FROM shows WHERE id = 62614;
 -- SELECT * FROM shows WHERE id = 63881;
 
-SELECT * FROM shows WHERE id IN
-(SELECT show_id FROM ratings WHERE rating >= 6.0 LIMIT 10);
+-- SELECT * FROM shows WHERE id IN
+-- (SELECT show_id FROM ratings WHERE rating >= 6.0 LIMIT 10);
+
+SELECT title, rating FROM shows JOIN ratings ON
+shows.id = ratings.show_id WHERE rating >= 6.0 LIMIT 10;
