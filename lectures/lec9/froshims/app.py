d658b2d447fa5219a519bfa6e26a0ddd61a6f206
Georgiy Eresko <zhorik.eresko@mail.ru>
2024-08-03 16:18:49 +0300

    update

--- lectures/lec9/froshims/app.py
+++ lectures/lec9/froshims/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-SPORTS = ["Basketball", "Volleyball", "Football"]
+SPORTS = ["Basketball", "Volleyball", "Football", "Boxing"]
 
 @app.route("/")
 def index():
@@ -10,6 +10,9 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
+    if not request.form.get("name"):
         return render_template("failure.html")
+    for sport in request.form.getlist("sport"):
+        if sport not in SPORTS:
+            return render_template("failure.html")
     return render_template("success.html")
